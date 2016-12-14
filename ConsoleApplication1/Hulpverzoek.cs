be836ae58492c60ecd667172160224367c15bf27
Tinglers <CATG.adams@student.han.nl>
2016-12-14 13:13:16 +0100

    opzet die http verzoek kan doen naar endpoint en resultaat kan parsen

---
+++ ConsoleApplication1/Hulpverzoek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Hulpverzoek
+    {
+        string BSN { get; set; }
+        string AmbassadeID { get; set; }
+        string onderwerp { get; set; }
+        string bericht {get; set;}
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+
 
 namespace ConsoleApplication1
 {
@@ -10,6 +12,50 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                using (HttpResponseMessage webresponse = getDaysTickets(DateTime.Today))
+                {
+                    HulpverzoekCollection verzoeken = Deserialize<HulpverzoekCollection>(webresponse.ToString());
+                }
+            }
+            
+
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            var obj = Activator.CreateInstance<T>();
+            var ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (T)serializer.ReadObject(ms);
+            ms.Close();
+            return obj;
+        }
+
+        static HttpResponseMessage getDaysTickets(DateTime date)
+        {
+            var endpoint = "";
+            var urlParameters = "";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(endpoint);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("http request succesfull");
+                return response;
+            }
+            else
+            {
+                Console.WriteLine("http request failed");
+                return null;
+            }
         }
     }
 }
